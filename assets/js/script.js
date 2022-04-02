acbbbee75852272405cabd161f1d52e6683b55b3
randronaco <randronaco1027@gmail.com>
2022-04-02 18:03:47 -0400

    added for loop to clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+let apiKey = "745b025da524f67261b6a7f65eb9dc66"
+let citySearchTerm = "Phoenix"
 
 // $("#cityBtn").click(function () {
 //     let citySearchTerm = $("#city-search-term").val()
@@ -12,126 +14,115 @@
 //         }).then(function (response) {
 // })
 
-var lat = "30.267151"
-var lon = "-97.743057"
-let apiKey = "745b025da524f67261b6a7f65eb9dc66"
-let citySearchTerm = "Beijing"
-
 let url = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearchTerm + "&Appid=" + apiKey + "&units=imperial"
-let urlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearchTerm + "&Appid=" + apiKey + "&units=imperial"
-
 
 fetch(url)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data)
-
-        var milliseconds = new Date(data.dt * 1000);
-        var currentDate = milliseconds.toLocaleDateString();
-        // var cityName = data.timezone.split("/")
-        var textDisplay = data.name + " (" + currentDate + ")"
-        $("#cityDisplay").text(textDisplay)
-        $("#temp").text(data.main.temp + "°F")
-        $("#wind").text(data.wind.speed + " MPH")
-        $("#humidity").text(data.main.humidity + "%")
-
-        console.log(data.coord.lat + " " + data.coord.lon)
         let urlUvi = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&appid=' + apiKey + '&units=imperial'
         fetch(urlUvi)
             .then(function (responseUvi) {
                 return responseUvi.json();
             })
-            .then(function (dataUvi) {
-                console.log(dataUvi)
-                $("#uvIndex").text(dataUvi.current.uvi)
+            .then(function (dataForecast) {
+                var milliseconds = new Date(data.dt * 1000);
+                var currentDate = milliseconds.toLocaleDateString();
+                // var cityName = data.timezone.split("/")
+                var textDisplay = data.name + " (" + currentDate + ")"
+                $("#cityDisplay").text(textDisplay)
+                $("#temp").text(data.main.temp + "°F")
+                $("#wind").text(data.wind.speed + " MPH")
+                $("#humidity").text(data.main.humidity + "%")
+                $("#uvIndex").text(dataForecast.current.uvi)
+                console.log(dataForecast)
+
                 // UV Index Color Coding
-                if (dataUvi.current.uvi < 3) {
+                if (dataForecast.current.uvi < 3) {
                     $("#uvIndex").addClass("uv-low")
                 }
-                else if (dataUvi.current.uvi >= 3 && dataUvi.current.uvi < 6) {
+                else if (dataForecast.current.uvi >= 3 && dataForecast.current.uvi < 6) {
                     $("#uvIndex").addClass("uv-moderate")
                 }
-                else if (dataUvi.current.uvi >= 6 && dataUvi.current.uvi < 8) {
+                else if (dataForecast.current.uvi >= 6 && dataForecast.current.uvi < 8) {
                     $("#uvIndex").addClass("uv-high")
                 }
-                else if (dataUvi.current.uvi >= 8 && dataUvi.current.uvi < 11) {
+                else if (dataForecast.current.uvi >= 8 && dataForecast.current.uvi < 11) {
                     $("#uvIndex").addClass("uv-very-high")
                 }
-                else if (dataUvi.current.uvi >= 11) {
+                else if (dataForecast.current.uvi >= 11) {
                     $("#uvIndex").addClass("uv-extreme")
                 }
+
+                // Emoji for Current Weather
+                if (data.weather[0].main === "Clear") {
+                    $("#cityDisplay").text(textDisplay + "☀️")
+                }
+                else if (data.weather[0].main === "Clouds") {
+                    $("#cityDisplay").text(textDisplay + "☁️")
+                }
+                else if (data.weather[0].main === "Rain") {
+                    $("#cityDisplay").text(textDisplay + "🌧️")
+                }
+                else if (data.weather[0].main === "Snow") {
+                    $("#cityDisplay").text(textDisplay + "❄️")
+                }
+
+                // Weather for 5 Day Forecast
+                let emoji = [];
+                let millisecondsForecast = []
+                let currentDateForecast = []
+                let textDisplayForecast = []
+                for (var i = 1; i < 6; i++) {
+                    millisecondsForecast[i] = new Date(dataForecast.daily[i].dt * 1000);
+                    currentDateForecast[i] = millisecondsForecast[i].toLocaleDateString();
+                    textDisplayForecast[i] = currentDateForecast[i]
+                    $("#day1").text(textDisplayForecast[1])
+                    $("#day2").text(textDisplayForecast[2])
+                    $("#day3").text(textDisplayForecast[3])
+                    $("#day4").text(textDisplayForecast[4])
+                    $("#day5").text(textDisplayForecast[5])
+
+                    if (dataForecast.daily[i].weather[0].main === "Clear") {
+                        emoji[i] = "☀️"
+                    }
+                    else if (dataForecast.daily[i].weather[0].main === "Clouds") {
+                        emoji[i] = "☁️"
+                    }
+                    else if (dataForecast.daily[i].weather[0].main === "Rain") {
+                        emoji[i] = "🌧️"
+                    }
+                    else if (dataForecast.daily[i].weather[0].main === "Snow") {
+                        emoji[i] = "❄️"
+                    }
+                    $("#day1Weather").text(emoji[1])
+                    $("#day2Weather").text(emoji[2])
+                    $("#day3Weather").text(emoji[3])
+                    $("#day4Weather").text(emoji[4])
+                    $("#day5Weather").text(emoji[5])
+                }
+                $("#temp1").text(dataForecast.daily[1].temp.day + "°F")
+                $("#wind1").text(dataForecast.daily[1].wind_speed + " MPH")
+                $("#humidity1").text(dataForecast.daily[1].humidity + "%")
+
+                $("#temp2").text(dataForecast.daily[2].temp.day + "°F")
+                $("#wind2").text(dataForecast.daily[2].wind_speed + " MPH")
+                $("#humidity2").text(dataForecast.daily[2].humidity + "%")
+
+                $("#temp3").text(dataForecast.daily[3].temp.day + "°F")
+                $("#wind3").text(dataForecast.daily[3].wind_speed + " MPH")
+                $("#humidity3").text(dataForecast.daily[3].humidity + "%")
+
+                $("#temp4").text(dataForecast.daily[4].temp.day + "°F")
+                $("#wind4").text(dataForecast.daily[4].wind_speed + " MPH")
+                $("#humidity4").text(dataForecast.daily[4].humidity + "%")
+
+                $("#temp5").text(dataForecast.daily[5].temp.day + "°F")
+                $("#wind5").text(dataForecast.daily[5].wind_speed + " MPH")
+                $("#humidity5").text(dataForecast.daily[5].humidity + "%")
             })
+    })
 
 
 
-
-        // Emoji for Weather
-        if (data.weather[0].main === "Clear") {
-            console.log("clear sky")
-            $("#cityDisplay").text(textDisplay + "☀️")
-        }
-        else if (data.weather[0].main === "Clouds") {
-            console.log("clouds")
-            $("#cityDisplay").text(textDisplay + "☁️")
-        }
-        else if (data.weather[0].main === "Rain") {
-            console.log("rain")
-            $("#cityDisplay").text(textDisplay + "🌧️")
-        }
-        else if (data.weather[0].main === "Snow") {
-            console.log("snow")
-            $("#cityDisplay").text(textDisplay + "❄️")
-        }
-
-        fetch(urlForecast)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (dataForecast) {
-                console.log(dataForecast)
-                var milliseconds7 = new Date(dataForecast.list[7].dt * 1000);
-                var currentDate7 = milliseconds7.toLocaleDateString();
-                var textDisplay7 = currentDate7
-                $("#day1").text(textDisplay7)
-                $("#temp1").text(dataForecast.list[7].main.temp + "°F")
-                $("#wind1").text(dataForecast.list[7].wind.speed + " MPH")
-                $("#humidity1").text(dataForecast.list[7].main.humidity + "%")
-
-                var milliseconds15 = new Date(dataForecast.list[15].dt * 1000);
-                var currentDate15 = milliseconds15.toLocaleDateString();
-                var textDisplay15 = currentDate15
-                $("#day2").text(textDisplay15)
-                $("#temp2").text(dataForecast.list[15].main.temp + "°F")
-                $("#wind2").text(dataForecast.list[15].wind.speed + " MPH")
-                $("#humidity2").text(dataForecast.list[15].main.humidity + "%")
-
-                var milliseconds23 = new Date(dataForecast.list[23].dt * 1000);
-                var currentDate23 = milliseconds23.toLocaleDateString();
-                var textDisplay23 = currentDate23
-                $("#day3").text(textDisplay23)
-                $("#temp3").text(dataForecast.list[23].main.temp + "°F")
-                $("#wind3").text(dataForecast.list[23].wind.speed + " MPH")
-                $("#humidity3").text(dataForecast.list[23].main.humidity + "%")
-
-                var milliseconds31 = new Date(dataForecast.list[31].dt * 1000);
-                var currentDate31 = milliseconds31.toLocaleDateString();
-                var textDisplay31 = currentDate31
-                $("#day4").text(textDisplay31)
-                $("#temp4").text(dataForecast.list[31].main.temp + "°F")
-                $("#wind4").text(dataForecast.list[31].wind.speed + " MPH")
-                $("#humidity4").text(dataForecast.list[31].main.humidity + "%")
-
-                var milliseconds39 = new Date(dataForecast.list[39].dt * 1000);
-                var currentDate39 = milliseconds39.toLocaleDateString();
-                var textDisplay39 = currentDate39
-                $("#day5").text(textDisplay39)
-                $("#temp5").text(dataForecast.list[39].main.temp + "°F")
-                $("#wind5").text(dataForecast.list[39].wind.speed + " MPH")
-                $("#humidity5").text(dataForecast.list[39].main.humidity + "%")
-
-            })
-
-
-    });
